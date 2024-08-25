ea5657ebbd98253d12dc7165ec72b9e0f5a580f9
Sushil Kumar Yadav <sushilyadukul@gmail.com>
2024-08-25 22:40:09 +0530

    Basic files added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+console.log(import.meta.env.VITE_APPWRITE_URL)  
+
+  return (
+    <>
+     <h1>Testing the blogweb.</h1>
+    </>
+  )
+}
+
+export default App

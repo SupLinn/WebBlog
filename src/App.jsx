d49f2d2edc10f155c3e786b516867dc5dc8a4039
SupLinn <148838687+SupLinn@users.noreply.github.com>
2024-08-27 23:57:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-
+import React, {useState, useEffect} from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth_service'
 import './App.css'
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)  
+// console.log(import.meta.env.VITE_APPWRITE_URL)  
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
+ 
+ useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if(userData) {
+      dispatch(login({userData}))
+    }
+    else {
+      dispatch(logout())
+    }
+  })
+  .finally (()=> setLoading(false))
+ }, []) 
 
-  return (
-    <>
-     <h1>Testing the blogweb.</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> <div className = 'w-full block'>
+      <Header />
+      <main>
+        {/* <Outlet /> */}
+      </main>
+      <Footer /></div> </div>
+  ) : null
 }
 
 export default App
